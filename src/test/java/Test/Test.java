2e6db2058ae8011829b8814019af72e18a509f0e
Rebekah Radcliffe <rebekah.zoe.radcliffe@gmail.com>
2019-12-04 07:59:16 +0000

    tests update

--- src/test/java/Test/Test.java
+++ src/test/java/Test/Test.java
@@ -9,23 +9,24 @@ public class Test {
 	@org.junit.Test
 	public void test() {
 		
-		GameMethod choice = new GameMethod("rock","paper");
+		GameMethod choice =  GameMethod();
+		GameMethod choice = GameMethod.getWinner("rock","paper");
 		Assert.assertEquals("Computer wins!", choice);
-		GameMethod choice2 = new GameMethod("rock","rock");
+		GameMethod choice2 = GameMethod.getWinner("rock","rock");
 		Assert.assertEquals("Draw!", choice2);
-		GameMethod choice3 = new GameMethod("rock","scissors");
+		GameMethod choice3 = GameMethod.getWinner("rock","scissors");
 		Assert.assertEquals("User wins!", choice3);
-	    GameMethod choice4 = new GameMethod("paper","rock");
+	    GameMethod choice4 = GameMethod.getWinner("paper","rock");
 	    Assert.assertEquals("User wins!", choice4);
-	    GameMethod choice5 = new GameMethod("paper","paper");
+	    GameMethod choice5 = GameMethod.getWinner("paper","paper");
 	    Assert.assertEquals("Draw!", choice5);
-	    GameMethod choice6 = new GameMethod("paper","scissors");
+	    GameMethod choice6 = GameMethod.getWinner("paper","scissors");
 	    Assert.assertEquals("Computer wins!", choice6);
-	    GameMethod choice7 = new GameMethod("scissors","paper");
+	    GameMethod choice7 = GameMethod.getWinner("scissors","paper");
 	    Assert.assertEquals("User wins!", choice7);
-	    GameMethod choice8 = new GameMethod("scissors","rock");
+	    GameMethod choice8 = GameMethod.getWinner("scissors","rock");
 	    Assert.assertEquals("Computer wins!", choice8);
-	    GameMethod choice9 = new GameMethod("scissors","scissors");
+	    GameMethod choice9 = GameMethod.getWinner("scissors","scissors");
 	    Assert.assertEquals("Draw!", choice9);
 	}
 }
